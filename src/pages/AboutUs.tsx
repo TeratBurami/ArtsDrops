d0bccdb997f294386bcc32fe6bb9dd8295c0d43a
Saannddy <pathompum47@gmail.com>
2024-02-29 11:08:54 +0700

    AboutUs updated

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -6,52 +6,71 @@ export default function AboutUs() {
         <>
             <Nav></Nav>
             
-            <ul className='mt-20'>
+            <ul className='mt-20 mb-20'>
                 <li>
-                    <div className="flex w-fit mx-auto">
+                    <div className="flex w-fit mx-auto items-center">
                         <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
-                        <div>
-                            <h2 className='font-semibold mt-10'> Terat Burami </h2>
-                            <p className='p-3 shadow-md w-[1000px] h-[200px]'>6588011</p>
-                        </div>    
+                            <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
+                                <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                                    <h2 className='font-light text-right mt-1'>- Terat Burami </h2>
+                                </p>
+                            </div>  
                     </div>
                 </li>
-                <li className='text-center'>
-                    <hr className='m-10 h-2'/>
-                </li>
+
+                {/*Line*/}
+                <li className='text-center'><hr className='m-10 ml-96 mr-96'/></li>
+
                 <li>
-                    <div className="flex w-fit mx-auto">
-                        <div>
-                            <h2 className='font-semibold float-right'>Pathompum Jirakarnpaisan</h2>
-                            <p className='p-3 shadow-md w-[1000px] h-[200px]'>
-                            6588029
+                    <div className="flex w-fit mx-auto itmes-center">
+                        <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
+                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                                <h2 className='font-light mt-1 text-right'>- Pathompum Jirakarpaisan</h2>
                             </p>
                         </div>   
                         <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="ml-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/> 
                     </div>
                 </li>
 
+                {/*Line*/}
+                <li className='text-center'> <hr className='m-10 ml-96 mr-96'/> </li>
+
                 <li>
-                    <div className="flex w-fit mx-auto">
+                    <div className="flex w-fit mx-auto items-center">
                         <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
-                        <div>
-                            <h2 className='font-semibold mt-10'> Patsawat Kittipunthopast </h2>
-                            <p className='p-3 shadow-md w-[1000px] h-[200px]'>6588011</p>
+                        <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
+                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                                <h2 className='font-light mt-1 text-right'>- Patsawat Kittipunthopast</h2>
+                            </p>
                         </div>    
                     </div>
                 </li>
-                <li className='text-center'>
-                    <hr className='m-10 h-2'/>
-                </li>
+                
+                {/*Line*/}
+                <li className='text-center'> <hr className='m-10 ml-96 mr-96'/> </li>
+
                 <li>
-                    <div className="flex w-fit mx-auto">
-                        <div>
-                            <h2 className='font-semibold float-right'>Pathompum Jirakarnpaisan</h2>
-                            <p className='p-3 shadow-md w-[1000px] h-[200px]'>
-                            6588029
+                    <div className="flex w-fit mx-auto items-center">
+                        <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
+                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                                <h2 className='font-light mt-1 text-right'>- Athichanan Rodusa</h2>
                             </p>
                         </div>   
                         <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="ml-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/> 
+                    </div>
+                </li>
+
+                {/*Line*/}
+                <li className='text-center'> <hr className='m-10 ml-96 mr-96'/> </li>
+
+                <li>
+                    <div className="flex w-fit mx-auto items-center">
+                        <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
+                        <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
+                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                                <h2 className='font-light mt-1 text-right'>- Nithimeth Mandee</h2>
+                            </p>
+                        </div>    
                     </div>
                 </li>
 
