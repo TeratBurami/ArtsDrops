074462ad717c9599b2d85ed06dd26f05b70efde2
Saannddy <pathompum47@gmail.com>
2024-02-29 15:08:03 +0700

    AboutUS Final with no Beam

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,5 +1,9 @@
 import Nav from '../components/nav/nav';
 import Footer from '../components/footer/footer'
+import Phoom from '../assets/images/Phoom.png'
+import Sea from '../assets/images/Sea.jpg'
+import Guy from '../assets/images/guy.png'
+import Keep from '../assets/images/keep.jpg'
 
 export default function AboutUs() {
     return (
@@ -9,9 +13,9 @@ export default function AboutUs() {
             <ul className='mt-20 mb-20'>
                 <li>
                     <div className="flex w-fit mx-auto items-center">
-                        <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
+                        <img src={Sea} alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
                             <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
-                                <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                                <p className='font-semibold'>"The more I think it over, the more I feel that there is nothing more truly artistic than to love people."
                                     <h2 className='font-light text-right mt-1'>- Terat Burami </h2>
                                 </p>
                             </div>  
@@ -24,11 +28,11 @@ export default function AboutUs() {
                 <li>
                     <div className="flex w-fit mx-auto items-center">
                         <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
-                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                            <p className='font-semibold'>"Roses are red, Violets are blue, White wine cost less, than dinner for two"
                                 <h2 className='font-light mt-1 text-right'>- Pathompum Jirakarpaisan</h2>
                             </p>
                         </div>   
-                        <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="ml-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/> 
+                        <img src={Phoom} alt="" className="ml-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/> 
                     </div>
                 </li>
 
@@ -37,9 +41,9 @@ export default function AboutUs() {
 
                 <li>
                     <div className="flex w-fit mx-auto items-center">
-                        <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
+                        <img src={Guy} alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
                         <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
-                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                            <p className='font-semibold'>"Im clueless why im here"
                                 <h2 className='font-light mt-1 text-right'>- Patsawat Kittipunthopast</h2>
                             </p>
                         </div>    
@@ -52,11 +56,11 @@ export default function AboutUs() {
                 <li>
                     <div className="flex w-fit mx-auto items-center">
                         <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
-                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                            <p className='font-semibold'>"Live your life and forget your age."
                                 <h2 className='font-light mt-1 text-right'>- Athichanan Rodusa</h2>
                             </p>
                         </div>   
-                        <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="ml-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/> 
+                        <img src={Keep} alt="" className="ml-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/> 
                     </div>
                 </li>
 
@@ -65,9 +69,9 @@ export default function AboutUs() {
 
                 <li>
                     <div className="flex w-fit mx-auto items-center">
-                        <img src="https://i.pinimg.com/736x/46/07/44/46074402e28fb6637d1f0f7ed11676e2.jpg" alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
+                        <img src={Sea} alt="" className="mr-40 p-3 shadow-md shadow-slate-400 mb-3 w-60"/>
                         <div className='w-[1000px] h-[200px] shadow-md p-3 flex items-center justify-center'>
-                            <p className='font-semibold'>"In this Part, It will be replace by some random quotes"
+                            <p className='font-semibold'>"Don't worry, Don't Cry Drink Vodka and fly!"
                                 <h2 className='font-light mt-1 text-right'>- Nithimeth Mandee</h2>
                             </p>
                         </div>    
