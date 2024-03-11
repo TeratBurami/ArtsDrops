4ab8f05225b8efe8ffff96ed7d8b769f981f9d0d
Saannddy <pathompum47@gmail.com>
2024-03-11 13:00:07 +0700

    commit

--- server/Database/insert.sql
+++ server/Database/insert.sql
@@ -11,3 +11,5 @@ INSERT INTO Art VALUES
 ('AR002', 'PalaceLearning', 'Painting', 8000.00, 2, 'PalaceLearning Cafe Terrace at Night Poster by Vincent Van Gogh - 1881 - Fine Art Print - The Cafe Terrace on The Place du Forum','https://i.pinimg.com/564x/3c/c7/af/3cc7af43775361a90a5c13ab2422ea9b.jpg', 'AT003'),
 ('AR003', 'Matisse','Painting',1800.00,3,'Why Was Matisse Obsessed with Goldfish?','https://i.pinimg.com/564x/43/4c/dc/434cdcac98e36e57e15048b9e0eadfb4.jpg', 'AT001'),
 ('AR004', 'Pin','Painting','4250.00',3,'Pin by Eve Ringers on Portrait | Art prints boho, Abstract art painting, Art inspiration painting','https://i.pinimg.com/564x/48/1f/a7/481fa74f181e64cf8578ea1b54c97076.jpg', 'AT003');
+
+COMMIT;
